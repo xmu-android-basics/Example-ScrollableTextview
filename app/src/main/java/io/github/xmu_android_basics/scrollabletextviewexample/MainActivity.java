ca6976d84b0d5008dffb72f0fa5949821be9b21c
吴德文 <winwdood@xmu.edu.cn>
2020-03-20 23:23:29 +0800

    问题的初始状态: 如何让 TextView 在垂直方向可以滚动?

---
+++ app/src/main/java/io/github/xmu_android_basics/scrollabletextviewexample/MainActivity.java
@@ -0,0 +1,29 @@
+package io.github.xmu_android_basics.scrollabletextviewexample;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    TextView largeText;
+
+    private int i;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        largeText = findViewById(R.id.large_text);
+
+        for (i = 0; i < 10; i++) {
+            largeText.append("\n" + i + ": Filling text...");
+        }
+    }
+
+    public void onClick(View view) {
+        largeText.append("\n" + i++ + ": More text...");
+    }
+}
